7db78067d7952929c39d0e67c8568010123321e1
mrjarvis698 <mail2mrjarvis698@gmail.com>
2023-03-07 15:13:28 +0530

    Automate:  Provide CPU and MOBO INFO for Registration. Signed-off-by: mrjarvis698-bot <mail2mrjarvis698@gmail.com>

--- Resources/Buy/Buy_UID.xaml.cs
+++ Resources/Buy/Buy_UID.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI.Xaml.Controls;
+using System.Management;
 namespace Automate.Resources.Buy;
 
 public partial class Buy_UID : ContentPage
@@ -5,5 +7,21 @@ public partial class Buy_UID : ContentPage
 	public Buy_UID()
 	{
 		InitializeComponent();
-	}
+        var CPU_List = new ManagementObjectSearcher("Select ProcessorID From Win32_processor").Get();
+        var Motherboard_List = new ManagementObjectSearcher("Select SerialNumber From Win32_BaseBoard").Get();
+
+        foreach (ManagementObject CPU in CPU_List)
+        {
+            CPU_prop.Text = CPU["ProcessorID"].ToString();
+        }
+        foreach (ManagementObject MotherBoard in Motherboard_List)
+        {
+            Board_prop.Text = MotherBoard["SerialNumber"].ToString();
+        }
+    }
+
+	private void btn_click(object sender, EventArgs e)
+	{
+        // TODO: Add Website URL Redirect for Registration.
+    }
 }
